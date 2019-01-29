3b12b297c0655fe02f9e8abbeaa0263ae7bd99cd
Matt Proctor <matthew.proctor@fibercore.com>
2019-01-29 16:00:29 +0000

    Added test loop, need DA

--- isogainEfficiency/isogainEfficiency/Form1.Designer.vb
+++ isogainEfficiency/isogainEfficiency/Form1.Designer.vb
@@ -1,9 +1,9 @@
-﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
 Partial Class Form1
     Inherits System.Windows.Forms.Form
 
     'Form overrides dispose to clean up the component list.
-    <System.Diagnostics.DebuggerNonUserCode()> _
+    <System.Diagnostics.DebuggerNonUserCode()>
     Protected Overrides Sub Dispose(ByVal disposing As Boolean)
         Try
             If disposing AndAlso components IsNot Nothing Then
@@ -20,156 +20,291 @@ Partial Class Form1
     'NOTE: The following procedure is required by the Windows Form Designer
     'It can be modified using the Windows Form Designer.  
     'Do not modify it using the code editor.
-    <System.Diagnostics.DebuggerStepThrough()> _
+    <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-		Me.GroupBox1 = New System.Windows.Forms.GroupBox()
-		Me.btnEngPOn = New System.Windows.Forms.Button()
-		Me.btnEngSOn = New System.Windows.Forms.Button()
-		Me.Label2 = New System.Windows.Forms.Label()
-		Me.Label1 = New System.Windows.Forms.Label()
-		Me.txtEngPCur = New System.Windows.Forms.TextBox()
-		Me.txtEngSCur = New System.Windows.Forms.TextBox()
-		Me.txtEngPPow = New System.Windows.Forms.TextBox()
-		Me.txtEngSPow = New System.Windows.Forms.TextBox()
-		Me.btnEngPSet = New System.Windows.Forms.Button()
-		Me.btnEngSSet = New System.Windows.Forms.Button()
-		Me.GroupBox1.SuspendLayout()
-		Me.SuspendLayout()
-		'
-		'GroupBox1
-		'
-		Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
-			Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
-		Me.GroupBox1.Controls.Add(Me.btnEngPOn)
-		Me.GroupBox1.Controls.Add(Me.btnEngSOn)
-		Me.GroupBox1.Controls.Add(Me.Label2)
-		Me.GroupBox1.Controls.Add(Me.Label1)
-		Me.GroupBox1.Controls.Add(Me.txtEngPCur)
-		Me.GroupBox1.Controls.Add(Me.txtEngSCur)
-		Me.GroupBox1.Controls.Add(Me.txtEngPPow)
-		Me.GroupBox1.Controls.Add(Me.txtEngSPow)
-		Me.GroupBox1.Controls.Add(Me.btnEngPSet)
-		Me.GroupBox1.Controls.Add(Me.btnEngSSet)
-		Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.GroupBox1.ForeColor = System.Drawing.Color.Red
-		Me.GroupBox1.Location = New System.Drawing.Point(12, 471)
-		Me.GroupBox1.Name = "GroupBox1"
-		Me.GroupBox1.Size = New System.Drawing.Size(690, 110)
-		Me.GroupBox1.TabIndex = 0
-		Me.GroupBox1.TabStop = False
-		Me.GroupBox1.Text = "Engineering control"
-		'
-		'btnEngPOn
-		'
-		Me.btnEngPOn.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.btnEngPOn.Location = New System.Drawing.Point(267, 51)
-		Me.btnEngPOn.Name = "btnEngPOn"
-		Me.btnEngPOn.Size = New System.Drawing.Size(128, 23)
-		Me.btnEngPOn.TabIndex = 9
-		Me.btnEngPOn.Text = "Switch pump ON"
-		Me.btnEngPOn.UseVisualStyleBackColor = True
-		'
-		'btnEngSOn
-		'
-		Me.btnEngSOn.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.btnEngSOn.Location = New System.Drawing.Point(267, 22)
-		Me.btnEngSOn.Name = "btnEngSOn"
-		Me.btnEngSOn.Size = New System.Drawing.Size(128, 23)
-		Me.btnEngSOn.TabIndex = 8
-		Me.btnEngSOn.Text = "Switch seed ON"
-		Me.btnEngSOn.UseVisualStyleBackColor = True
-		'
-		'Label2
-		'
-		Me.Label2.AutoSize = True
-		Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.Label2.Location = New System.Drawing.Point(504, 82)
-		Me.Label2.Name = "Label2"
-		Me.Label2.Size = New System.Drawing.Size(76, 13)
-		Me.Label2.TabIndex = 7
-		Me.Label2.Text = "Pump power"
-		'
-		'Label1
-		'
-		Me.Label1.AutoSize = True
-		Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.Label1.Location = New System.Drawing.Point(504, 24)
-		Me.Label1.Name = "Label1"
-		Me.Label1.Size = New System.Drawing.Size(74, 13)
-		Me.Label1.TabIndex = 6
-		Me.Label1.Text = "Seed power"
-		'
-		'txtEngPCur
-		'
-		Me.txtEngPCur.Location = New System.Drawing.Point(6, 51)
-		Me.txtEngPCur.Name = "txtEngPCur"
-		Me.txtEngPCur.Size = New System.Drawing.Size(100, 22)
-		Me.txtEngPCur.TabIndex = 5
-		'
-		'txtEngSCur
-		'
-		Me.txtEngSCur.Location = New System.Drawing.Point(6, 23)
-		Me.txtEngSCur.Name = "txtEngSCur"
-		Me.txtEngSCur.Size = New System.Drawing.Size(100, 22)
-		Me.txtEngSCur.TabIndex = 4
-		'
-		'txtEngPPow
-		'
-		Me.txtEngPPow.Location = New System.Drawing.Point(584, 82)
-		Me.txtEngPPow.Name = "txtEngPPow"
-		Me.txtEngPPow.Size = New System.Drawing.Size(100, 22)
-		Me.txtEngPPow.TabIndex = 3
-		'
-		'txtEngSPow
-		'
-		Me.txtEngSPow.Location = New System.Drawing.Point(584, 24)
-		Me.txtEngSPow.Name = "txtEngSPow"
-		Me.txtEngSPow.Size = New System.Drawing.Size(100, 22)
-		Me.txtEngSPow.TabIndex = 2
-		'
-		'btnEngPSet
-		'
-		Me.btnEngPSet.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.btnEngPSet.Location = New System.Drawing.Point(112, 50)
-		Me.btnEngPSet.Name = "btnEngPSet"
-		Me.btnEngPSet.Size = New System.Drawing.Size(118, 23)
-		Me.btnEngPSet.TabIndex = 1
-		Me.btnEngPSet.Text = "Set pump current"
-		Me.btnEngPSet.UseVisualStyleBackColor = True
-		'
-		'btnEngSSet
-		'
-		Me.btnEngSSet.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-		Me.btnEngSSet.Location = New System.Drawing.Point(112, 23)
-		Me.btnEngSSet.Name = "btnEngSSet"
-		Me.btnEngSSet.Size = New System.Drawing.Size(118, 23)
-		Me.btnEngSSet.TabIndex = 0
-		Me.btnEngSSet.Text = "Set seed current"
-		Me.btnEngSSet.UseVisualStyleBackColor = True
-		'
-		'Form1
-		'
-		Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
-		Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-		Me.ClientSize = New System.Drawing.Size(714, 593)
-		Me.Controls.Add(Me.GroupBox1)
-		Me.Name = "Form1"
-		Me.Text = "Form1"
-		Me.GroupBox1.ResumeLayout(False)
-		Me.GroupBox1.PerformLayout()
-		Me.ResumeLayout(False)
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.btnEngPOn = New System.Windows.Forms.Button()
+        Me.btnEngSOn = New System.Windows.Forms.Button()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.txtEngPCur = New System.Windows.Forms.TextBox()
+        Me.txtEngSCur = New System.Windows.Forms.TextBox()
+        Me.txtEngPPow = New System.Windows.Forms.TextBox()
+        Me.txtEngSPow = New System.Windows.Forms.TextBox()
+        Me.btnEngPSet = New System.Windows.Forms.Button()
+        Me.btnEngSSet = New System.Windows.Forms.Button()
+        Me.tmrMain = New System.Windows.Forms.Timer(Me.components)
+        Me.cmbTest = New System.Windows.Forms.ComboBox()
+        Me.btnStartTest = New System.Windows.Forms.Button()
+        Me.tmrTest = New System.Windows.Forms.Timer(Me.components)
+        Me.txtTestPPower = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.btnStopTest = New System.Windows.Forms.Button()
+        Me.Label5 = New System.Windows.Forms.Label()
+        Me.txtTestSPower = New System.Windows.Forms.TextBox()
+        Me.cmb2 = New System.Windows.Forms.ComboBox()
+        Me.cmb1 = New System.Windows.Forms.ComboBox()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Anchor = CType(((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.btnEngPOn)
+        Me.GroupBox1.Controls.Add(Me.btnEngSOn)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Controls.Add(Me.txtEngPCur)
+        Me.GroupBox1.Controls.Add(Me.txtEngSCur)
+        Me.GroupBox1.Controls.Add(Me.txtEngPPow)
+        Me.GroupBox1.Controls.Add(Me.txtEngSPow)
+        Me.GroupBox1.Controls.Add(Me.btnEngPSet)
+        Me.GroupBox1.Controls.Add(Me.btnEngSSet)
+        Me.GroupBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.GroupBox1.ForeColor = System.Drawing.Color.Red
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 471)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(690, 110)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "Engineering control"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Font = New System.Drawing.Font("Microsoft Sans Serif", 9.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label3.ForeColor = System.Drawing.Color.Black
+        Me.Label3.Location = New System.Drawing.Point(68, 26)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(25, 15)
+        Me.Label3.TabIndex = 1
+        Me.Label3.Text = "mA"
+        '
+        'btnEngPOn
+        '
+        Me.btnEngPOn.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEngPOn.Location = New System.Drawing.Point(267, 51)
+        Me.btnEngPOn.Name = "btnEngPOn"
+        Me.btnEngPOn.Size = New System.Drawing.Size(128, 23)
+        Me.btnEngPOn.TabIndex = 9
+        Me.btnEngPOn.Text = "Switch pump ON"
+        Me.btnEngPOn.UseVisualStyleBackColor = True
+        '
+        'btnEngSOn
+        '
+        Me.btnEngSOn.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEngSOn.Location = New System.Drawing.Point(267, 22)
+        Me.btnEngSOn.Name = "btnEngSOn"
+        Me.btnEngSOn.Size = New System.Drawing.Size(128, 23)
+        Me.btnEngSOn.TabIndex = 8
+        Me.btnEngSOn.Text = "Switch seed ON"
+        Me.btnEngSOn.UseVisualStyleBackColor = True
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label2.Location = New System.Drawing.Point(504, 82)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(76, 13)
+        Me.Label2.TabIndex = 7
+        Me.Label2.Text = "Pump power"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.Location = New System.Drawing.Point(504, 24)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(74, 13)
+        Me.Label1.TabIndex = 6
+        Me.Label1.Text = "Seed power"
+        '
+        'txtEngPCur
+        '
+        Me.txtEngPCur.Location = New System.Drawing.Point(6, 51)
+        Me.txtEngPCur.Name = "txtEngPCur"
+        Me.txtEngPCur.Size = New System.Drawing.Size(56, 22)
+        Me.txtEngPCur.TabIndex = 5
+        '
+        'txtEngSCur
+        '
+        Me.txtEngSCur.Location = New System.Drawing.Point(6, 23)
+        Me.txtEngSCur.Name = "txtEngSCur"
+        Me.txtEngSCur.Size = New System.Drawing.Size(56, 22)
+        Me.txtEngSCur.TabIndex = 4
+        '
+        'txtEngPPow
+        '
+        Me.txtEngPPow.Location = New System.Drawing.Point(584, 82)
+        Me.txtEngPPow.Name = "txtEngPPow"
+        Me.txtEngPPow.ReadOnly = True
+        Me.txtEngPPow.Size = New System.Drawing.Size(100, 22)
+        Me.txtEngPPow.TabIndex = 3
+        '
+        'txtEngSPow
+        '
+        Me.txtEngSPow.Location = New System.Drawing.Point(584, 24)
+        Me.txtEngSPow.Name = "txtEngSPow"
+        Me.txtEngSPow.ReadOnly = True
+        Me.txtEngSPow.Size = New System.Drawing.Size(100, 22)
+        Me.txtEngSPow.TabIndex = 2
+        '
+        'btnEngPSet
+        '
+        Me.btnEngPSet.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEngPSet.Location = New System.Drawing.Point(112, 50)
+        Me.btnEngPSet.Name = "btnEngPSet"
+        Me.btnEngPSet.Size = New System.Drawing.Size(118, 23)
+        Me.btnEngPSet.TabIndex = 1
+        Me.btnEngPSet.Text = "Set pump current"
+        Me.btnEngPSet.UseVisualStyleBackColor = True
+        '
+        'btnEngSSet
+        '
+        Me.btnEngSSet.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnEngSSet.Location = New System.Drawing.Point(112, 23)
+        Me.btnEngSSet.Name = "btnEngSSet"
+        Me.btnEngSSet.Size = New System.Drawing.Size(118, 23)
+        Me.btnEngSSet.TabIndex = 0
+        Me.btnEngSSet.Text = "Set seed current"
+        Me.btnEngSSet.UseVisualStyleBackColor = True
+        '
+        'tmrMain
+        '
+        Me.tmrMain.Interval = 250
+        '
+        'cmbTest
+        '
+        Me.cmbTest.FormattingEnabled = True
+        Me.cmbTest.Location = New System.Drawing.Point(468, 147)
+        Me.cmbTest.Name = "cmbTest"
+        Me.cmbTest.Size = New System.Drawing.Size(121, 21)
+        Me.cmbTest.TabIndex = 1
+        '
+        'btnStartTest
+        '
+        Me.btnStartTest.Location = New System.Drawing.Point(100, 104)
+        Me.btnStartTest.Name = "btnStartTest"
+        Me.btnStartTest.Size = New System.Drawing.Size(142, 23)
+        Me.btnStartTest.TabIndex = 2
+        Me.btnStartTest.Text = "Start Efficiency Test"
+        Me.btnStartTest.UseVisualStyleBackColor = True
+        '
+        'tmrTest
+        '
+        Me.tmrTest.Interval = 1000
+        '
+        'txtTestPPower
+        '
+        Me.txtTestPPower.Location = New System.Drawing.Point(319, 104)
+        Me.txtTestPPower.Name = "txtTestPPower"
+        Me.txtTestPPower.Size = New System.Drawing.Size(100, 20)
+        Me.txtTestPPower.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(316, 88)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(85, 13)
+        Me.Label4.TabIndex = 4
+        Me.Label4.Text = "980 power [mW]"
+        '
+        'btnStopTest
+        '
+        Me.btnStopTest.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnStopTest.ForeColor = System.Drawing.Color.Red
+        Me.btnStopTest.Location = New System.Drawing.Point(111, 205)
+        Me.btnStopTest.Name = "btnStopTest"
+        Me.btnStopTest.Size = New System.Drawing.Size(75, 23)
+        Me.btnStopTest.TabIndex = 5
+        Me.btnStopTest.Text = "Stop Test"
+        Me.btnStopTest.UseVisualStyleBackColor = True
+        '
+        'Label5
+        '
+        Me.Label5.AutoSize = True
+        Me.Label5.Location = New System.Drawing.Point(316, 147)
+        Me.Label5.Name = "Label5"
+        Me.Label5.Size = New System.Drawing.Size(91, 13)
+        Me.Label5.TabIndex = 7
+        Me.Label5.Text = "1550 power [mW]"
+        '
+        'txtTestSPower
+        '
+        Me.txtTestSPower.Location = New System.Drawing.Point(319, 163)
+        Me.txtTestSPower.Name = "txtTestSPower"
+        Me.txtTestSPower.Size = New System.Drawing.Size(100, 20)
+        Me.txtTestSPower.TabIndex = 6
+        '
+        'cmb2
+        '
+        Me.cmb2.FormattingEnabled = True
+        Me.cmb2.Location = New System.Drawing.Point(441, 286)
+        Me.cmb2.Name = "cmb2"
+        Me.cmb2.Size = New System.Drawing.Size(121, 21)
+        Me.cmb2.TabIndex = 8
+        '
+        'cmb1
+        '
+        Me.cmb1.FormattingEnabled = True
+        Me.cmb1.Location = New System.Drawing.Point(297, 286)
+        Me.cmb1.Name = "cmb1"
+        Me.cmb1.Size = New System.Drawing.Size(121, 21)
+        Me.cmb1.TabIndex = 9
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(714, 593)
+        Me.Controls.Add(Me.cmb1)
+        Me.Controls.Add(Me.cmb2)
+        Me.Controls.Add(Me.Label5)
+        Me.Controls.Add(Me.txtTestSPower)
+        Me.Controls.Add(Me.btnStopTest)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.txtTestPPower)
+        Me.Controls.Add(Me.btnStartTest)
+        Me.Controls.Add(Me.cmbTest)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Isogain efficiency"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
 
-	End Sub
+    End Sub
 
-	Friend WithEvents GroupBox1 As GroupBox
-	Friend WithEvents Label2 As Label
-	Friend WithEvents Label1 As Label
-	Friend WithEvents txtEngPCur As TextBox
-	Friend WithEvents txtEngSCur As TextBox
-	Friend WithEvents txtEngPPow As TextBox
-	Friend WithEvents txtEngSPow As TextBox
-	Friend WithEvents btnEngPSet As Button
-	Friend WithEvents btnEngSSet As Button
-	Friend WithEvents btnEngPOn As Button
-	Friend WithEvents btnEngSOn As Button
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+    Friend WithEvents txtEngPCur As TextBox
+    Friend WithEvents txtEngSCur As TextBox
+    Friend WithEvents txtEngPPow As TextBox
+    Friend WithEvents txtEngSPow As TextBox
+    Friend WithEvents btnEngPSet As Button
+    Friend WithEvents btnEngSSet As Button
+    Friend WithEvents btnEngPOn As Button
+    Friend WithEvents btnEngSOn As Button
+    Friend WithEvents tmrMain As Timer
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cmbTest As ComboBox
+    Friend WithEvents btnStartTest As Button
+    Friend WithEvents tmrTest As Timer
+    Friend WithEvents txtTestPPower As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents btnStopTest As Button
+    Friend WithEvents Label5 As Label
+    Friend WithEvents txtTestSPower As TextBox
+    Friend WithEvents cmb2 As ComboBox
+    Friend WithEvents cmb1 As ComboBox
 End Class
--- isogainEfficiency/isogainEfficiency/Form1.vb
+++ isogainEfficiency/isogainEfficiency/Form1.vb
@@ -1,61 +1,172 @@
-﻿'Imports Thorlabs.PM100D_32.Interop
-'Imports Thorlabs.TL4000
+﻿Imports Thorlabs.PM100D_32.Interop
+Imports Thorlabs.TL4000
 
 Public Class Form1
-	'Dim lasSeed, lasPump As TL4000
-	'Dim pmSeed, pmOut As PM100D
-	Dim seedOn As Boolean = False 'is the laser on or off ?
+    Dim lasSeed, lasPump As TL4000
+    Dim pm980 As New PM100D("USB0::0x1313::0x8072::P2008967::0::INSTR", True, True)
+    Dim pm1550 As New PM100D("USB0::0x1313::0x8072::P2007348::0::INSTR", True, True)
+    Dim seedOn As Boolean = False 'is the laser on or off ?
 	Dim pumpOn As Boolean = False
-	Dim SCur, PCur As Double
+    Dim SCur, PCur As Double
+    Dim calB1, calIntercept As Double
+    Dim listPumpPower As New List(Of Double)(New Double() {100, 200, 300, 400, 500, 600})
+    Dim listPumpCurrent As New List(Of Double)
+    Dim testCount, testIndex, testSwitch As Int16
+    Dim pwr1550Test, pwr980Test As Double
+    Dim pwr980, pwr1550 As Decimal
+    Dim list980Power, list1550Power As New List(Of Double)
+
+    'Dim lisOut, lisSeed, lisPump As New List(Of Double)
 
 
 
-	Private Sub btnEngSOn_Click(sender As Object, e As EventArgs) Handles btnEngSOn.Click
+
+
+
+    Private Sub btnEngSOn_Click(sender As Object, e As EventArgs) Handles btnEngSOn.Click
 		If seedOn = False Then
 			'MsgBox("Ensure that lid is closed")
 		End If
 
 		seedOn = Not (seedOn)
-		If seedOn = True Then
-			btnEngSOn.Text = "Switch seed OFF"
-		ElseIf seedOn = False Then
-			btnEngSOn.Text = "Switch seed ON"
-		End If
-	End Sub
-	Private Sub btnEngPOn_Click(sender As Object, e As EventArgs) Handles btnEngPOn.Click
-		If pumpOn = False Then
-			MsgBox("Ensure that lid is closed")
-		End If
+        If seedOn = True Then
+            btnEngSOn.Text = "Switch seed OFF"
+            txtEngSPow.Text = "test"
+            lasSeed.switchLdOutput(True)
+        ElseIf seedOn = False Then
+            btnEngSOn.Text = "Switch seed ON"
+            lasSeed.switchLdOutput(False)
+        End If
+    End Sub
+    Private Sub btnEngPOn_Click(sender As Object, e As EventArgs) Handles btnEngPOn.Click
+        If pumpOn = False Then
+            MsgBox("Ensure that lid is closed")
+        End If
 
-		pumpOn = Not (pumpOn)
-		If pumpOn = True Then
-			btnEngPOn.Text = "Switch pump OFF"
-		ElseIf pumpOn = False Then
-			btnEngPOn.Text = "Switch pump ON"
-		End If
-	End Sub
+        pumpOn = Not (pumpOn)
+        If pumpOn = True Then
+            btnEngPOn.Text = "Switch pump OFF"
+            lasPump.switchLdOutput(True)
+        ElseIf pumpOn = False Then
+            btnEngPOn.Text = "Switch pump ON"
+            lasPump.switchLdOutput(False)
+        End If
+    End Sub
 
-	Dim lisOut, lisSeed, lisPump As New List(Of Double)
+    Private Sub tmrMain_Tick(sender As Object, e As EventArgs) Handles tmrMain.Tick
+        pm1550.measPower(pwr1550)
+        pm980.measPower(pwr980)
+        pwr1550 = Decimal.Round(pwr1550 * 1000, 1)
+        pwr980 = Decimal.Round(pwr980 * 1000, 1)
 
-	Private Sub txtEngSCur_TextChanged(sender As Object, e As EventArgs) Handles txtEngSCur.TextChanged
-		'SCur = txtEngSCur.Text
-	End Sub
-	Private Sub txtEngSCur_keyPress(sender As Object, e As KeyPressEventArgs) Handles txtEngSCur.KeyPress
+        txtTestPPower.Text = pwr980
+        txtTestSPower.Text = pwr1550
+    End Sub
 
-		If Char.IsControl(e.KeyChar) And Char.IsDigit(e.KeyChar) And e.KeyChar = "." Then
-			e.Handled = True
-		Else
-			e.Handled = False
-		End If
+    Private Sub btnEngSSet_Click(sender As Object, e As EventArgs) Handles btnEngSSet.Click
+        Dim seedCur As Double
+        seedCur = txtEngSCur.Text / 1000
+        lasSeed.setLdCurrSetpoint(seedCur)
+    End Sub
 
-		'// only allow one decimal point
-		'If ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
-		'	{
-		'	e.Handled = True;
-		'  }
-	End Sub
+    Private Sub btnStopTest_Click(sender As Object, e As EventArgs) Handles btnStopTest.Click
+        tmrTest.Stop()
+        testIndex = 0
+        lasPump.setLdCurrSetpoint(0.01)
+        lasPump.switchLdOutput(0)
+        lasSeed.switchLdOutput(0)
+        For Each entry In list1550Power
+            cmb1.Items.Add(entry)
+        Next
+        For Each entry In list980Power
+            cmb2.Items.Add(entry)
+        Next
 
-	Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
-		'PCur = txtEngPCur.Text
-	End Sub
+    End Sub
+
+    Private Sub btnStartTest_Click(sender As Object, e As EventArgs) Handles btnStartTest.Click
+        tmrTest.Start()
+    End Sub
+
+
+
+    Private Sub btnEngPSet_Click(sender As Object, e As EventArgs) Handles btnEngPSet.Click
+        Dim pumpCur As Double
+        pumpCur = txtEngPCur.Text / 1000
+        lasPump.setLdCurrSetpoint(pumpCur)
+    End Sub
+
+    Private Sub tmrTest_Tick(sender As Object, e As EventArgs) Handles tmrTest.Tick
+        If testSwitch = 0 Then
+            testIndex = 0
+            lasPump.setLdCurrSetpoint(listPumpCurrent(0))
+            lasSeed.switchLdOutput(1)
+            lasPump.switchLdOutput(1)
+
+        End If
+        testSwitch = 1
+        If testCount = 10 Then
+            list980Power.Add(pwr980)
+            list1550Power.Add(pwr1550)
+            testIndex += 1
+            Dim pumpCurrent As Double = listPumpCurrent(testIndex)
+            lasPump.setLdCurrSetpoint(pumpCurrent)
+            testCount = 0
+
+        End If
+        If testIndex = 6 Then
+            lasPump.setLdCurrSetpoint(0.01)
+            lasPump.switchLdOutput(0)
+            lasSeed.switchLdOutput(0)
+            tmrTest.Stop()
+            testSwitch = 0
+        End If
+        testCount += 1
+
+
+    End Sub
+
+    Private Sub txtEngSCur_keyPress(sender As Object, e As KeyPressEventArgs) Handles txtEngSCur.KeyPress
+
+        If Char.IsControl(e.KeyChar) And Char.IsDigit(e.KeyChar) And e.KeyChar = "." Then
+            e.Handled = True
+        Else
+            e.Handled = False
+        End If
+
+        '// only allow one decimal point
+        'If ((e.KeyChar == '.') && ((sender as TextBox).Text.IndexOf('.') > -1))
+        '	{
+        '	e.Handled = True;
+        '  }
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        'PCur = txtEngPCur.Text
+        tmrMain.Start()
+        lasSeed = New TL4000("USB::4883::32847::M00280750::INSTR", True, False)
+        lasPump = New TL4000("USB0::0x1313::0x804F::M00280970::0::INSTR", True, False)
+        lasSeed.getLdCurrSetpoint(0, SCur)
+        lasPump.getLdCurrSetpoint(0, PCur)
+        txtEngSCur.Text = SCur * 1000
+        txtEngPCur.Text = PCur * 1000
+        lasPump.setLdCurrLimit(1.1)
+        pm980.setWavelength(980)
+        pm1550.setWavelength(1550)
+        calB1 = 0.699
+        calIntercept = -41.047
+        For Each num In listPumpPower
+            listPumpCurrent.Add((num - calIntercept) / (1000 * calB1))
+        Next
+        For Each num In listPumpCurrent
+            cmbTest.Items.Add(num)
+        Next
+        testSwitch = 0
+
+
+    End Sub
+    Private Sub Form1_close(sender As Object, e As EventArgs) Handles MyBase.Closed
+        lasSeed.switchLdOutput(0)
+        lasPump.switchLdOutput(0)
+    End Sub
 End Class
